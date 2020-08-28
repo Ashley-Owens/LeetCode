757e298581d4427a814e1ba6b12835e6d84e33b2
booleancube <ashleyowenspt@gmail.com>
2020-08-28 08:19:18 -0700

    Added isHappy leetcode problem

--- HashTables.py
+++ HashTables.py
@@ -30,6 +30,25 @@ class Solution:
         # Sorts the values of each dictionary, comparing the results.
         return sorted(d1.values()) == sorted(d2.values())
 
+    def isHappy(self, n: int) -> bool:
+        """
+        Starting with any positive integer, replace the number by the sum of the squares of its digits, 
+        and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in 
+        a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+        Args:
+            n (int): starting integer
+        Returns:
+            bool: True if n is a happy number, else False
+        """
+        # Creates a set for tracking numbers that have been encountered.
+        seen = set()
+
+        while n not in seen:
+            seen.add(n)
+            n = sum(int(x) **2 for x in str(n))
+        
+        return n == 1
+
 
 
 
@@ -37,7 +56,10 @@ if __name__ == "__main__":
     
     sol = Solution()
     
-    # Tests isIsomorphic method
-    print(sol.isIsomorphic("foo", "bar"))
-    print(sol.isIsomorphic("paper", "title"))
+    # # Tests isIsomorphic method
+    # print(sol.isIsomorphic("foo", "bar"))
+    # print(sol.isIsomorphic("paper", "title"))
 
+
+    # Tests Jewels in Stones method
+    print(sol.isHappy(2))
