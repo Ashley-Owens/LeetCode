943297f9335cf086e0188635fcc7cd6e03c4974c
booleancube <ashleyowenspt@gmail.com>
2020-10-15 09:55:17 -0700

    makeChange code comments complete

--- DP.py
+++ DP.py
@@ -22,6 +22,75 @@ def cutRod(prices, n, cost):
     
     
     
-arr = [1, 5, 8, 9, 10, 17, 17, 20, 24, 30] 
-size = len(arr) 
-print("Maximum Obtainable Value is " + str(cutRod(arr, size, 1))) 
+# arr = [1, 5, 8, 9, 10, 17, 17, 20, 24, 30] 
+# size = len(arr) 
+# print("Maximum Obtainable Value is " + str(cutRod(arr, size, 1))) 
+
+
+
+
+def makeChange(V, A):
+    """
+    Making Change: Given coins of denominations (value) 1 = v1 < v2 < ... < vn, we wish to make change for an amount A using as few coins as possible. Assume that vi’s and A are integers. Since v1= 1 there will always be a solution. Formally, an algorithm for this problem should take as input an array V where V[i] is the value of the coin of the ith denomination and a value A which is the amount of change we are asked to make. The algorithm should return an array C where C[i] is the number of coins of value V[i] to return as change and m the minimum number of coins it took. You must return exact change with the minimum number of coins.
+    Args:
+        V (list): an array of coin denominations
+        A (int): the target number to make change for
+    Returns:
+        list: an array of the number of minimum coins required for each denomination to make change for A.
+    """
+    
+    table = [[0 for i in range(A+1)] for j in range(len(V))]
+    C = [0] * len(V) 
+      
+    # Initializes matrix, storing minimum coins required for each row (V[i]).
+    for i in range(len(V)): 
+        
+      
+      # Columns: total number of subproblems or A
+        for j in range(A+1):
+        
+            # Populates first column with zeros
+            if j == 0:
+                table[i][j] = 0
+            
+            # Populates first row with 0 – len(A) since V[0] equals 1.
+            elif V[i] == 1:
+                table[i][j] = j
+    
+            else:
+                # The coin denomination is too large, so just take the previously determined least coinage.
+                if V[i] > j:
+                    table[i][j] = table[i-1][j]
+                 
+                # Calculates min coins based on current and previous values
+                else:
+                    target = j - V[i]
+                    minCoins = table[i][target] +1
+    
+                    # Need to determine if current minimum number of coins is less than previously found value.
+                    if table[i-1][j] < minCoins:
+                        table[i][j] = table[i-1][j]
+                    else:
+                        table[i][j] = minCoins
+    print(table)
+    # Populates C array with the minimum coins for each V[i].
+    i = len(V)-1
+    j = A
+    minCoins = table[i][j]
+
+    while j > 0:
+    
+        if table[i-1][j] > minCoins:
+            j = j - V[i]
+            C[i] += 1
+            minCoins = table[i][j]
+        
+        else:
+            i -= 1
+             
+    return C
+    
+A = 20
+V = [1, 5]
+
+print(makeChange(V, A))
