fb1f995627660d92d49bd8a6674fea26d733b0af
Ashley <ashleyowenspt@gmail.com>
2021-07-11 08:33:20 -0700

    added stack and queue methods

--- StacksQueues.py
+++ StacksQueues.py
@@ -18,6 +18,8 @@
 # Queue Problem Solving Options
 # 1. Useful for ordering elements in FIFO order
 
+from queue import Queue
+
 
 def postfix(string):
     """
@@ -59,6 +61,72 @@ def postfix(string):
     return stack.pop()
 
 
+def isValid(s):
+    dict = {')': '(', '}': '{', ']':'['}
+    stack = []
+
+    for char in s:
+        
+        if char not in dict:
+            stack.append(char)
+        
+        elif char in dict:
+            if stack:
+                if stack.pop() != dict[char]:
+                    return False
+            else:
+                return False
+    
+    return len(stack) == 0
+
+
+def reverseFirstKElements(q, k):
+    """Reverse first k elements of a queue. If k is greater than the
+    number of elements in the input, return the original queue.
+
+    Args:
+        q (Queue): Python object filled with integers
+        k (int): number of items to reverse
+
+    Returns:
+        Queue: Python object in proper order
+    """
+    size = q.qsize()
+    stack = []
+    
+    if k > size or k <= 0:
+        return q
+    
+    if not q:
+        return q
+    
+    for i in range(k):
+        stack.append(q.get())
+        
+    while stack:
+        q.put(stack.pop())
+        
+    for _ in range(size - k):
+        num = q.get()
+        q.put(num)
+    
+    return q
+
+
 # Output: 14
 print(postfix("5 1 2  +  4  * +  3  -"))
 
+# Output False
+print(isValid('([)]'))
+# Output: True
+print(isValid('{[]}'))
+
+# Input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] and k=4
+# Output [4, 3, 2, 1, 5, 6, 7, 8, 9, 10]
+q = Queue(maxsize=10)
+items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for i in items:
+    q.put(i)
+# print(list(q.queue))
+res = reverseFirstKElements(q, 4)
+print(list(res.queue))
